5603b379cc463a6102f4dfe440312a8f6fb72403
brandonsr <brandon2004roldan@gmail.com>
2021-04-14 21:00:33 -0600

    Add capacity to work with float numbers and refresh button

--- foot.js
+++ foot.js
@@ -1,9 +1,10 @@
 let getData = function(){
-    let km = parseInt(document.getElementById('form').value)
+    let km = parseFloat(document.getElementById('form').value)
     console.log(km + 10)
     let r = km / 3
     if(km>0){
-        document.write('Tu resultado es: '+ r +" yardas")
+        document.write('<h1>Tu resultado es: '+ r +" yardas</h1>")
+        let button = document.write("<button onclick='window.location.reload(true)'>Convertir otros datos</button>")
     }
     else{
         document.write('insert data')
--- km.js
+++ km.js
@@ -1,9 +1,10 @@
 let getData = function(){
-    let km = parseInt(document.getElementById('form').value)
+    let km = parseFloat(document.getElementById('form').value)
     console.log(km + 10)
     let r = km * 1000
     if(km>0){
-        document.write('Tu resultado es: '+ r +" metros")
+        document.write('<h1>Tu resultado es: '+ r +' metros</h1>')
+        let button = document.write("<button onclick='window.location.reload(true)'>Convertir otros datos</button>")
     }
     else{
         document.write('insert data')
